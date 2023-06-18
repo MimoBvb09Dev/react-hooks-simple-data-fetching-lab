7fb596337ad4ac78e340e376275a0ae71231e3e5
moh sid <sid-rah@live.fr>
2023-06-18 11:42:22 -0700

    update components

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setImageSrc(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => console.warn(error));
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={imageSrc} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
